b4948dc596b20145ebd135ebea0a616ec1068dc6
Philippe van Amerongen <philippe.vanamerongen@gmail.com>
2022-03-31 17:09:29 +0200

    Update index.ts

--- src/FigmaDriver/index.ts
+++ src/FigmaDriver/index.ts
@@ -1,43 +1,24 @@
 /*
 |--------------------------------------------------------------------------
-| Ally Oauth driver
+| Ally Oauth Figma driver
 |--------------------------------------------------------------------------
 |
-| This is a dummy implementation of the Oauth driver. Make sure you
+| This is a Figma implementation of the Oauth driver.
 |
-| - Got through every line of code
-| - Read every comment
 |
 */
 
-import type { AllyUserContract } from '@ioc:Adonis/Addons/Ally'
+import type { ApiRequestContract } from '@ioc:Adonis/Addons/Ally'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import { Oauth2Driver, ApiRequest, RedirectRequest } from '@adonisjs/ally/build/standalone'
-import { FigmaUserResponse } from './FigmaUserResponse'
+import { Oauth2Driver, RedirectRequest } from '@adonisjs/ally/build/standalone'
 import { FigmaScopes } from './FigmaScopes'
+import { FigmaUserResponse } from './FigmaUserResponse'
 
-/**
- * Define the access token object properties in this type. It
- * must have "token" and "type" and you are free to add
- * more properties.
- *
- * ------------------------------------------------
- * Change "FigmaDriver" to something more relevant
- * ------------------------------------------------
- */
 export type FigmaDriverAccessToken = {
   token: string
   type: 'bearer'
 }
 
-/**
- * Define the configuration options accepted by the figma driver. It must have the following
- * properties and you are free add more.
- *
- * ------------------------------------------------
- * Change "FigmaDriver" to something more relevant
- * ------------------------------------------------
- */
 export type FigmaDriverConfig = {
   driver: 'figma'
   clientId: string
@@ -48,112 +29,25 @@ export type FigmaDriverConfig = {
   userInfoUrl?: string
 }
 
-/**
- * Driver implementation. It is mostly configuration driven except the user calls
- *
- * ------------------------------------------------
- * Change "FigmaDriver" to something more relevant
- * ------------------------------------------------
- */
 export class FigmaDriver extends Oauth2Driver<FigmaDriverAccessToken, FigmaScopes> {
-  /**
-   * The URL for the redirect request. The user will be redirected on this page
-   * to authorize the request.
-   *
-   * Do not define query strings in this URL.
-   */
   protected authorizeUrl = 'https://www.figma.com/oauth'
 
-  /**
-   * The URL to hit to exchange the authorization code for the access token
-   *
-   * Do not define query strings in this URL.
-   */
   protected accessTokenUrl = 'https://www.figma.com/api/oauth/token'
 
-  /**
-   * The URL to hit to get the user details
-   *
-   * Do not define query strings in this URL.
-   */
-  protected userInfoUrl = 'https://www.figma.com/api/v1/me'
+  protected userInfoUrl = 'https://api.figma.com/v1/me'
 
-  /**
-   * The param name for the authorization code. Read the documentation of the figma oauth
-   * provider and update the param name to match the query string field name in
-   * which the oauth provider sends the authorization_code post redirect.
-   */
   protected codeParamName = 'code'
 
-  /**
-   * The param name for the error. Read the documentation of the figma oauth provider and update
-   * the param name to match the query string field name in which the oauth provider sends
-   * the error post redirect
-   */
   protected errorParamName = 'error'
 
-  /**
-   * Cookie name for storing the CSRF token. Make sure it is always unique. So a better
-   * approach is to prefix the oauth provider name to `oauth_state` value. For example:
-   * For example: "facebook_oauth_state"
-   */
-  protected stateCookieName = 'FigmaDriver_oauth_state'
+  protected stateCookieName = 'figma_oauth_state'
 
-  /**
-   * Parameter name to be used for sending and receiving the state from.
-   * Read the documentation of the figma oauth provider and update the param
-   * name to match the query string used by the provider for exchanging
-   * the state.
-   */
   protected stateParamName = 'state'
 
-  /**
-   * Parameter name for sending the scopes to the oauth provider.
-   */
   protected scopeParamName = 'scope'
 
-  /**
-   * The separator indentifier for defining multiple scopes
-   */
   protected scopesSeparator = ' '
 
-  constructor(ctx: HttpContextContract, public config: FigmaDriverConfig) {
-    super(ctx, config)
-
-    /**
-     * Extremely important to call the following method to clear the
-     * state set by the redirect request.
-     *
-     * DO NOT REMOVE THE FOLLOWING LINE
-     */
-    this.loadState()
-  }
-
-  /**
-   * Optionally configure the authorization redirect request. The actual request
-   * is made by the base implementation of "Oauth2" driver and this is a
-   * hook to pre-configure the request.
-   */
-  // protected configureRedirectRequest(request: RedirectRequest<FigmaScopes>) {}
-
-  /**
-   * Optionally configure the access token request. The actual request is made by
-   * the base implementation of "Oauth2" driver and this is a hook to pre-configure
-   * the request
-   */
-  // protected configureAccessTokenRequest(request: ApiRequest) {}
-
-  /**
-   * Update the implementation to tell if the error received during redirect
-   * means "ACCESS DENIED".
-   */
-  public accessDenied() {
-    return this.ctx.request.input('error') === 'user_denied'
-  }
-
-  /**
-   * Returns the HTTP request with the authorization header set
-   */
   protected getAuthenticatedRequest(url: string, token: string) {
     const request = this.httpClient(url)
     request.header('Authorization', `Bearer ${token}`)
@@ -167,64 +61,55 @@ export class FigmaDriver extends Oauth2Driver<FigmaDriverAccessToken, FigmaScope
     request.param('response_type', 'code')
   }
 
-  /**
-   * Get the user details by query the provider API. This method must return
-   * the access token and the user details both. Checkout the google
-   * implementation for same.
-   *
-   * https://github.com/adonisjs/ally/blob/develop/src/Drivers/Google/index.ts#L191-L199
-   */
-  public async user(
-    callback?: (request: ApiRequest) => void
-  ): Promise<AllyUserContract<FigmaDriverAccessToken>> {
-    const accessToken = await this.accessToken()
-    const request = this.httpClient(this.config.userInfoUrl || this.userInfoUrl)
+  constructor(ctx: HttpContextContract, public config: FigmaDriverConfig) {
+    super(ctx, config)
 
-    /**
-     * Allow end user to configure the request. This should be called after the figma driver
-     * configuration, so that the user can override them (if required)
-     */
-    if (typeof callback === 'function') {
-      callback(request)
-    }
-
-    /**
-     * Write the implementation details here
-     */
-    const user = await this.userFromToken(accessToken.token, callback)
-
-    return user
+    this.loadState()
   }
 
-  public async userFromToken(
-    accessToken: string,
-    callback?: (request: ApiRequest) => void
-  ): Promise<AllyUserContract<{ token: string; type: 'bearer' }>> {
-    const request = this.httpClient(this.config.userInfoUrl || this.userInfoUrl)
-
-    /**
-     * Allow end user to configure the request. This should be called after the figma driver
-     * configuration, so that the user can override them (if required)
-     */
+  protected async getUserInfo(token: string, callback?: (request: ApiRequestContract) => void) {
+    const request = this.getAuthenticatedRequest(this.config.userInfoUrl || this.userInfoUrl, token)
     if (typeof callback === 'function') {
       callback(request)
     }
 
-    /**
-     * Write the implementation details here
-     */
-
     const body: FigmaUserResponse = await request.get()
-
     return {
       id: body.id,
-      nickName: body.handle,
       name: body.handle,
-      email: body.email,
-      emailVerificationState: 'unsupported',
+      nickName: body.handle,
       avatarUrl: body.img_url,
-      token: { token: accessToken, type: 'bearer' },
+      email: body.email,
+      emailVerificationState: 'unsupported' as 'unsupported',
       original: body,
+    }
+  }
+
+  public accessDenied(): boolean {
+    const error = this.getError()
+    if (!error) {
+      return false
+    }
+
+    return error === 'access_denied'
+  }
+
+  public async user(callback?: (request: ApiRequestContract) => void) {
+    const token = await this.accessToken(callback)
+    const user = await this.getUserInfo(token.token, callback)
+
+    return {
+      ...user,
+      token,
+    }
+  }
+
+  public async userFromToken(token: string, callback?: (request: ApiRequestContract) => void) {
+    const user = await this.getUserInfo(token, callback)
+
+    return {
+      ...user,
+      token: { token, type: 'bearer' as const },
     }
   }
 }
